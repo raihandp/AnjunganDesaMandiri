4d6e7d3543e10ab3ae83f848f8e70744644f831e
raihandp <rdpringgodigdo@gmail.com>
2024-11-22 14:08:46 +0700

    cek route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\Surat_Digital\skDomisiliController;
 use App\Http\Controllers\SuratController;
 
 
-Route::get('/login', [LoginController::class, 'showNikForm'])->name('login.showNikForm');
-Route::post('/login/check-nik', [LoginController::class, 'checkNik'])->name('login.checkNik');
-Route::get('/login/pin/{nik}', [LoginController::class, 'showPinForm'])->name('login.showPinForm');
-Route::post('/login/check-pin', [LoginController::class, 'checkPin'])->name('login.checkPin');
+// Route::get('/login', [LoginController::class, 'showNikForm'])->name('login.showNikForm');
+// Route::post('/login/check-nik', [LoginController::class, 'checkNik'])->name('login.checkNik');
+// Route::get('/login/pin/{nik}', [LoginController::class, 'showPinForm'])->name('login.showPinForm');
+// Route::post('/login/check-pin', [LoginController::class, 'checkPin'])->name('login.checkPin');
 
 Route::get('/', function () {
     return view('halaman_utama');
@@ -24,10 +24,10 @@ Route::get('/profil_desa', function () {
 });
 
 Route::get('/surat_keterangan_domisili', [skDomisiliController::class, 'showForm']);
-Route::post('/sk-domisili/submit', [SuratController::class, 'submitForm']);
+// Route::post('/sk-domisili/submit', [skDomisiliController::class, 'submitForm']);
 
-Route::get('/surat-domisili', [SuratController::class, 'showForm'])->name('surat.showForm');
-Route::post('/sk-domisili/submit', [SuratController::class, 'submitForm'])->name('surat.submitForm');
+// Route::get('/surat-domisili', [SuratController::class, 'showForm'])->name('surat.showForm');
+// Route::post('/sk-domisili/submit', [SuratController::class, 'submitForm'])->name('surat.submitForm');
 
 // Route::get('/surat_keterangan_domisili', function () {
 //     return view('surat_digital.skd');
