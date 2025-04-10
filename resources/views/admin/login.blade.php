7ea2aa7090cd4c3b7bb8d1f0fc0c5d0d839e469f
raihandp <rdpringgodigdo@gmail.com>
2025-04-10 15:34:36 +0700

    update layout menu_surat

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Admin - Anjungan Desa Mandiri</title>
-    <link rel="icon" href="{{ asset('assets/logo.png') }}" type="image/png">
     <style>
         body {
             margin: 0;
--- resources/views/layout/admin/menu_surat.blade.php
+++ resources/views/layout/admin/menu_surat.blade.php
@@ -1,5 +1,5 @@
 <div class="menu-surat">
-    <a href='{{route('layanan-surat-kelola-surat')}}' class="{{request()->routeIs('layanan-surat-kelola-surat') ? 'active' : ''}}">Kelola Surat</a>
+    <!-- <a href='{{route('layanan-surat-kelola-surat')}}' class="{{request()->routeIs('layanan-surat-kelola-surat') ? 'active' : ''}}">Kelola Surat</a> -->
     <a href='{{route('layanan-surat-dalam-proses')}}'
         class="{{request()->routeIs('layanan-surat-dalam-proses*') ? 'active' : ''}}">Dalam Proses</a>
     <a href='{{route('layanan-surat-ditolak')}}' class="{{request()->routeIs('layanan-surat-ditolak') ? " active" : ''
