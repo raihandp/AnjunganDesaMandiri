e5ebe28449937636c0e974155ae2b5eb84318231
raihandp <rdpringgodigdo@gmail.com>
2025-04-11 03:52:17 +0700

    update view verif surat

--- resources/views/warga/layanan-mandiri/pilih-surat.blade.php
+++ resources/views/warga/layanan-mandiri/pilih-surat.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Layanan Mandiri - Anjungan Desa Mandiri Desa Rawapanjang</title>
-    <link rel="icon" href="{{ asset('assets/Logo-Kab-Bogor.png') }}" type="image/png">
+    <link rel="icon" href="{{asset('assets/logo.png')}}" type="image/png">
     <style>
         body {
             margin: 0;
