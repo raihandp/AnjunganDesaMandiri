204a4e9eb1b71ca643b4925ddb41b744c0dde06f
raihandp <rdpringgodigdo@gmail.com>
2025-04-11 03:54:27 +0700

    update view warga - berhasil

--- resources/views/warga/layanan-mandiri/berhasil.blade.php
+++ resources/views/warga/layanan-mandiri/berhasil.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Berhasil</title>
+    <link rel="icon" href="{{asset('assets/logo.png')}}" type="image/png">
     <style> 
         body { 
             display: flex;
