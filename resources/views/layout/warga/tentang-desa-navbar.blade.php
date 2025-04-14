b59c78c53f3620911c275e9952fa311d08a9dc0a
raihandp <rdpringgodigdo@gmail.com>
2025-04-14 14:28:36 +0700

    update view layout warga - tentang desa navbar

--- resources/views/layout/warga/tentang-desa-navbar.blade.php
+++ resources/views/layout/warga/tentang-desa-navbar.blade.php
@@ -6,9 +6,9 @@
             class="footer-button {{ request()->routeIs('sejarah-desa') ? 'active' : '' }}">Sejarah Desa <br>
             Rawapanjang</a>
         <a href='{{route('visi-misi')}}' class="footer-button {{ request()->routeIs('visi-misi') ? 'active' : '' }}">Visi Misi <br> Desa</a>
-        <a href='{{route('potensi-desa')}}' class="footer-button {{ request()->routeIs('potensi-desa') ? 'active' : '' }}">Potensi Desa</a>
+        <!-- <a href='{{route('potensi-desa')}}' class="footer-button {{ request()->routeIs('potensi-desa') ? 'active' : '' }}">Potensi Desa</a>
         <a href='{{route('statistik-desa')}}' class="footer-button {{ request()->routeIs('statistik-desa') ? 'active' : '' }}">Statistik Desa</a>
         <a href="{{route('kerjasama')}}" class="footer-button {{ request()->routeIs('kerjasama') ? 'active' : '' }}">Kerjasama</a>
-        <a href="{{route('kabar-pembangunan')}}" class="footer-button {{ request()->routeIs('kabar-pemangunan') ? 'active' : '' }}">Kabar Pembangunan</a>
+        <a href="{{route('kabar-pembangunan')}}" class="footer-button {{ request()->routeIs('kabar-pemangunan') ? 'active' : '' }}">Kabar Pembangunan</a> -->
     </div>
 </div>
